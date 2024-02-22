7e7d83e10a82c0ef6675f6b00cda145189e16c8a
Farhod <wedsdsa22@gmail.com>
2024-02-21 23:35:23 -0800

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './customer/components/Navigation/Navigation';
+import { HomePage } from './customer/pages/HomePage/HomePage';
+
+
+function App() {
+  return (
+    <><div className="">
+      <Navigation />
+    </div>
+    <div>
+      <HomePage />
+    </div></>
+  );
+}
+
+export default App;
---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainCarousel from '../../components/HomeCarousel/MainCarousel'
+import HomeSectionCarousel from '../../components/HomeSectionCarousel/HomeSectionCarousel'
+
+export const HomePage = () => {
+  return (
+    <><div>
+      <MainCarousel />
+    </div><div>
+        <HomeSectionCarousel />
+      </div></>
+  )
+}
