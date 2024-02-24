2904b3d19ed1a86bf091c3086d7e55c2030ecdf9
Farhod <wedsdsa22@gmail.com>
2024-02-24 04:23:36 -0800

    added product card filter

--- src/customer/components/Product/ProductCard.jsx
+++ src/customer/components/Product/ProductCard.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
+import "./ProductCard.css"
 
-const ProductCard = () => {
+
+const ProductCard = ({product}) => {
   return (
     <div className='productCard w-[15rem] m-3 transition-all cursor-pointer'>
         <div className='h-[20rem]'>
             <img className='h-full w-full object-cover object-left-top'
-             src="https://rukminim1.flixcart.com/image/612/612/xif0q/kurta/g/6/k/m-sksh-dt1105-pcbl-fubar-original-imafux247zhqym2z-bb.jpeg?q=70" alt="" />
+             src={product.imageUrl} alt="" />
         </div>
 
         <div className='textPart bg-white p-3'>
             <div>
-                <p className='font-bold opacity-60'>FUBAR</p>
-                <p>Men Printed Cotton Blend Straight Kurta</p>
+                <p className='font-bold opacity-60'>{product.brand}</p>
+                <p>{product.title}</p>
             </div>
             <div className='flex items-center space-x-2'>
-              <p className='font-semibold'>$199</p>
-              <p className='line-through opacity-50'>$1999</p>
-              <p className='text-green-600 font-semibold'>70% off</p>
+              <p className='font-semibold'>{product.discountedPrice}</p>
+              <p className='line-through opacity-50'>{product.price}</p>
+              <p className='text-green-600 font-semibold'>{product.discountPersent}% off</p>
             </div>
 
         </div>
