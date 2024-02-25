5e153b3be096f23c7c6f425eb4423b1591f85a1e
Farhod <wedsdsa22@gmail.com>
2024-02-24 23:10:38 -0800

    added functionality ProductDetails

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Footer from './customer/components/Footer/Footer';
 import Navigation from './customer/components/Navigation/Navigation';
 import Product from './customer/components/Product/Product';
+import ProductDetails from './customer/components/ProductDetails/ProductDetails';
 import { HomePage } from './customer/pages/HomePage/HomePage';
 
 
@@ -12,7 +13,8 @@ function App() {
     </div>
     <div>
       {/* <HomePage /> */}
-      <Product />
+      {/* <Product /> */}
+    <ProductDetails/>
     </div>
     <div>
       <Footer />
