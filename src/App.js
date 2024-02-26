c6b10c6231a72507e169aadae5a75f8793637624
Farhod <wedsdsa22@gmail.com>
2024-02-26 06:22:22 -0800

    Added react files AddressCard, Cart, Checkout, Order, ProductReviewCard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
+import Cart from './customer/components/Cart/Cart';
+import Checkout from './customer/components/Checkout/Checkout';
 import Footer from './customer/components/Footer/Footer';
 import Navigation from './customer/components/Navigation/Navigation';
+import Order from './customer/components/Order/Order';
 import Product from './customer/components/Product/Product';
 import ProductDetails from './customer/components/ProductDetails/ProductDetails';
 import { HomePage } from './customer/pages/HomePage/HomePage';
@@ -14,7 +17,10 @@ function App() {
     <div>
       {/* <HomePage /> */}
       {/* <Product /> */}
-    <ProductDetails/>
+    {/* <ProductDetails/> */}
+    {/* <Cart /> */}
+    {/* <Checkout/> */}
+    <Order/>
     </div>
     <div>
       <Footer />
---
+++ src/customer/components/AddressCard/AddressCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const AddressCard = () => {
+  return (
+    <div>
+
+        <div className='space-y-3'>
+
+            <p className='font-semibold'>Dilmurod Bekmametov</p>
+            <p>Tashkent Uzbekistan</p>
+            <div className='space-y-1'>
+                <p className='font-semibold'>Phone Number</p>
+                <p>887776655</p>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default AddressCard
